63b3f588cc58785403ad1e665c1c1e2b0e433020
Waterhorse928 <72754883+Waterhorse928@users.noreply.github.com>
2021-09-19 16:21:53 -0700

    added a lot not gonna explain

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 import os
+import sys
 import battle
 pygame.font.init()
 pygame.mixer.init()
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 1000, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("TBS Crossover Arena")
 
-BLACK = ( 0, 0, 0)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-RED = ( 255, 0, 0)
+RED = (255, 0, 0)
+DARK_BLUE = (0,8,53)
 
 FONT = pygame.font.SysFont('consolas', 20)
+
+CHEN_IMAGE = pygame.image.load(os.path.join('images', 'chen-trimmy.png'))
+BACKGROUND = pygame.image.load(os.path.join('images', 'Background.png'))
+BOX = pygame.image.load(os.path.join('images', 'box.png'))
+LOWER_BOX = pygame.image.load(os.path.join('images', 'lower_box2.png'))
+HIGHER_BOX = pygame.image.load(os.path.join('images', 'higher_box.png'))
 
 FPS = 60
 
@@ -25,13 +33,19 @@ def main():
     run = True
     while run:
         clock.tick(FPS)
-        text("test",(10,10),WHITE)
+        WIN.blit(BACKGROUND,(0,0))
+        WIN.blit(LOWER_BOX,(50,700))
+        WIN.blit(HIGHER_BOX,(50,50))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-
+            if event.type == pygame.KEYDOWN:
+                print (f"{0}: You pressed {event.key}")
+            if event.type == pygame.KEYUP:
+                print (f"{0}: You released {event.key}")
+            
 
 main()
 
