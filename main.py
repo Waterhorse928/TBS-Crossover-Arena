66093d9d8f5f3151b49ff064ed3edd4bb57b61bb
Waterhorse928 <72754883+Waterhorse928@users.noreply.github.com>
2021-09-20 19:35:45 -0700

    hehehhehehhehehhe ui

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import os
 import sys
+
+from pygame.constants import K_DOWN, K_LEFT, K_RIGHT, K_UP
 import battle
 pygame.font.init()
 pygame.mixer.init()
@@ -23,36 +25,92 @@ BOX = pygame.image.load(os.path.join('images', 'box.png'))
 LOWER_BOX = pygame.image.load(os.path.join('images', 'lower_box2.png'))
 HIGHER_BOX = pygame.image.load(os.path.join('images', 'higher_box.png'))
 BUTTON = pygame.image.load(os.path.join('images', 'button.png'))
+BUTTON_OUTLINE = pygame.image.load(os.path.join('images', 'button_outline.png'))
 
 FPS = 60
 
 def text(t, pos, color):
     WIN.blit (FONT.render(t,0,color), pos)
 
+
+
+
 def main():
     clock = pygame.time.Clock()
     run = True
+    x = 1
+    y = 1
+    menu = 1
     while run:
         clock.tick(FPS)
-        WIN.blit(BACKGROUND,(0,0))
-        WIN.blit(LOWER_BOX,(50,700))
-        WIN.blit(HIGHER_BOX,(50,50))
-        WIN.blit(BUTTON,(75,725))
-        WIN.blit(BUTTON,(375,725))
-        WIN.blit(BUTTON,(675,725))
-        WIN.blit(BUTTON,(75,850))
-        WIN.blit(BUTTON,(375,850))
-        WIN.blit(BUTTON,(675,850))
-        text("Skill 1", (90, 740), WHITE)
-        pygame.display.update()
+
+        if menu == 1:
+            #Varibles
+            y_limit_lower = 1
+            y_limit_upper = 2
+
+            x_limit_lower = 1
+            x_limit_upper = 3
+
+            #Display
+            WIN.blit(BACKGROUND,(0,0))
+    
+            WIN.blit(LOWER_BOX,(50,700))
+            WIN.blit(HIGHER_BOX,(50,50))
+    
+            if y == 1 and x == 1:
+                WIN.blit(BUTTON_OUTLINE, (70,720))
+            WIN.blit(BUTTON,(75,725))
+            text("Skill", (90, 740), WHITE)
+    
+            if y == 1 and x == 2:
+                WIN.blit(BUTTON_OUTLINE, (370,720))
+            WIN.blit(BUTTON,(375,725))
+    
+            if y == 1 and x == 3:
+                WIN.blit(BUTTON_OUTLINE, (670,720))
+            WIN.blit(BUTTON,(675,725))
+            
+            if y == 2 and x == 1:
+                WIN.blit(BUTTON_OUTLINE, (70,845))
+            WIN.blit(BUTTON,(75,850))
+    
+            if y == 2 and x == 2:
+                WIN.blit(BUTTON_OUTLINE, (370,845))
+            WIN.blit(BUTTON,(375,850))
+    
+            if y == 2 and x == 3:
+                WIN.blit(BUTTON_OUTLINE, (670,845))
+            WIN.blit(BUTTON,(675,850))
+
+            pygame.display.update()
+            
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
-                print (f"{0}: You pressed {event.key}")
-            if event.type == pygame.KEYUP:
-                print (f"{0}: You released {event.key}")
+                if event.key == K_DOWN:
+                    y += 1
+                    if y > y_limit_upper:
+                        y = y_limit_lower
+                if event.key == K_UP:
+                    y -= 1
+                    if y < y_limit_lower:
+                        y = y_limit_upper
+                if event.key == K_RIGHT:
+                    x += 1
+                    if x > x_limit_upper:
+                        x = x_limit_lower
+                if event.key == K_LEFT:
+                    x -= 1
+                    if x < x_limit_lower:
+                        x = x_limit_upper
+
+
+                
+            
             
 
 main()
