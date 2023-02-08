6895d9f896efa8ac2f4c62f4f6c0767382a5de17
Waterhorse928 <72754883+Waterhorse928@users.noreply.github.com>
2023-02-08 15:47:38 -0800

    NotChoosen Update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from characters import wikiList
 teamPickMode = 0
 playerA = ["","","","","","","","",""]
 playerB = []
-choosen = []
 
 def ask (lowRange,highRange):
     while True:
@@ -32,16 +31,16 @@ teamPickMode = ask(1,2)
 #replace choosen with a list of character not choosen yet. For x in list of not choosen.
 if teamPickMode == 1:
     print("Player A: Choose a character")
+    notChoosen = [*range(1,len(wikiList))]
     for y in range(1,9):
-        for x in range(1,len(wikiList)):
-            if not x in choosen:
-                print(f"{x}. {wikiList[x].name}")
-        n = ask(1,12)
-        choosen.append(n)
+        for x in notChoosen:
+            print(f"{x}. {wikiList[x].name}")
+        n = askList(notChoosen)
+        notChoosen.remove(n)
         playerA[y] = wikiList[n]
         print (f"Selected {wikiList[n].name}")
     for x in playerA:
         if x != "":
             print(f"{x.name}")
 elif teamPickMode == 2:
-    print ("filth")
+    print ("filth fool")
