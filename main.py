ef02aa2716e6da46652249e3fb5a92d9373bfeff
Waterhorse928 <72754883+Waterhorse928@users.noreply.github.com>
2023-02-08 22:06:39 -0800

    COLUMNS

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import math
 from characters import wikiList
 
 teamPickMode = 0
-playerA = ["","","","","","","","",""]
-playerB = []
+playerA = ["Player A","","","","","","","",""]
+playerB = ["Player B","","","","","","","",""]
 
 def ask (lowRange,highRange):
     while True:
@@ -25,22 +25,37 @@ def askList (numberList):
         if result in numberList:
             return result
         
+def listPick (player):
+    wikiListTaken = [*range(1,len(wikiList))]
+    for y in range(1,9):
+        print(f"{player[0]}: Choose a character")
+        for x in wikiListTaken:
+            print(f"{x}. {wikiList[x].name}")
+        n = askList(wikiListTaken)
+        wikiListTaken.remove(n)
+        player[y] = wikiList[n]
+        print (f"Selected {player[y].name}")
+    return player
+        
 print ("-Team Pick-\n 1. Draft\n 2. List")
 teamPickMode = ask(1,2)   
 
-#replace choosen with a list of character not choosen yet. For x in list of not choosen.
 if teamPickMode == 1:
-    print("Player A: Choose a character")
-    notChoosen = [*range(1,len(wikiList))]
+    wikiListA = [*range(1,len(wikiList))]
     for y in range(1,9):
-        for x in notChoosen:
+        print("Player A: Choose a character")
+        for x in wikiListA:
             print(f"{x}. {wikiList[x].name}")
-        n = askList(notChoosen)
-        notChoosen.remove(n)
+        n = askList(wikiListA)
+        wikiListA.remove(n)
         playerA[y] = wikiList[n]
-        print (f"Selected {wikiList[n].name}")
+        print (f"Selected {playerA[y].name}")
     for x in playerA:
         if x != "":
             print(f"{x.name}")
+
 elif teamPickMode == 2:
-    print ("filth fool")
+    playerA = listPick(playerA)
+    playerB = listPick(playerB)
+    for x in range(1,9):
+        print(f'{x}.{playerA[x].name: <{20}}{x}.{playerB[x].name: <{20}}')
